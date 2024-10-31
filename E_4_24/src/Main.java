90b15f65bf945c2b698d4cc607a0f6eed4d3f4f7
PuaMR <paumocrec@alu.edu.gva.es>
2024-10-31 08:33:18 +0100

    subir a mi repo

--- E_4_24/src/Main.java
+++ E_4_24/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+
+        int n = 696969;
+        int a = scanner.nextInt();
+
+
+        while (a != -1) {
+            n = scanner.nextInt();
+            if (n != -1) {
+                System.out.println(n - a);
+                a = n;
+            } else if (n == -1) {
+                break;
+            }
+        }
     }
 }
